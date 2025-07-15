a79a234f5e3088ee216e01b382a6a08ba3da6e48
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-07-15 20:55:07 +0530

    Add book model

---
+++ BookStore.Api/src/BookStore.Api.Domain/Models/Book.cs
@@ -0,0 +1,13 @@
+namespace BookStore.Api.Domain.Models;
+
+public class Book
+{
+    public string Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Author { get; set; }
+    
+    public string Category { get; set; }
+    
+}
